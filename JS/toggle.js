9104c9f4165eb38a56d35d73cbbf89255b84dde9
Claudevspace <claudia.tracanaa@gmail.com>
2025-05-13 17:33:42 -0300

    opacidad del fondo cambiada cuando se abre el menu

--- JS/toggle.js
+++ JS/toggle.js
@@ -1,12 +1,22 @@
 function toggleMenu() {
   const menu = document.getElementById('menu');
+  const titulo = document.getElementById('titulo');
+  const main = document.getElementById('main');
+  const footer = document.getElementById('footer');
   menu.classList.toggle('active'); // Alterna la clase 'active'
   
   // Cambia el display directamente
   if (menu.classList.contains('active')) {
-      menu.style.display = 'block'; // Muestra el menú
+      menu.style.display = 'flex'; // Muestra el menú
+      titulo.classList.add('opacidad');
+      main.classList.add('opacidad');
+      footer.classList.add('opacidad');
+      
   } else {
       menu.style.display = 'none'; // Oculta el menú
+      titulo.classList.remove('opacidad');
+      main.classList.remove('opacidad');
+      footer.classList.remove('opacidad');
   }
 }
 
