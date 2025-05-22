c71d7c3240a977fe2505469067a154347266f550
Claudevspace <claudia.tracanaa@gmail.com>
2025-05-21 21:07:15 -0300

    ultimo 21-05

--- JS/animaciones.js
+++ JS/animaciones.js
@@ -1,15 +1,37 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const boxes = document.querySelectorAll('.box');
 
-    function checkVisibility() {
-        boxes.forEach(box => {
-            const boxPosition = box.getBoundingClientRect();
-            if (boxPosition.top < window.innerHeight && boxPosition.bottom >= 0) {
-                box.classList.add('visible');
+
+document.addEventListener("DOMContentLoaded", function() {
+    const animateItems = document.querySelectorAll('.animate-item');
+
+    const options = {
+        root: null, 
+        rootMargin: '0px',
+        threshold: 0.1 
+    };
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                const animationClass = entry.target.getAttribute('data-animation');
+                
+                if (entry.target.classList.contains('sequential-animate')) {
+                    const index = Array.prototype.indexOf.call(animateItems, entry.target);
+                    setTimeout(() => {
+                        entry.target.style.opacity = 1; 
+                        entry.target.classList.add('animate__animated', animationClass); 
+                    }, index * 100); 
+                } else {
+                    entry.target.style.opacity = 1; 
+                    entry.target.classList.add('animate__animated', animationClass); 
+                }
+
+                observer.unobserve(entry.target); 
             }
         });
-    }
+    }, options);
 
-    document.addEventListener('scroll', checkVisibility);
-    checkVisibility(); // Verifica la visibilidad al cargar la página
+    animateItems.forEach(item => {
+        observer.observe(item); 
+    });
 });
+
